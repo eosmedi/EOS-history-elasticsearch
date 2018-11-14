e5759145fcfcdb014ccda0d9146b9810bbd105d1
fun <lljxx1@gmail.com>
2018-11-14 13:13:51 +0800

    account history

--- node.js
+++ node.js
@@ -29,7 +29,7 @@ fibos.load("emitter");
 
 
 var elasticWriteStream = require('./lib/elasticsearch.js');
-var writeStream = new elasticWriteStream(10, 'test', 'test');
+var writeStream = new elasticWriteStream(1000, 'test', 'test');
 
 fibos.on('action', (message) => {
 	
