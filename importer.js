d6eba18a6b457fdf97230d34afb3cb640fb2883b
fun <lljxx1@gmail.com>
2018-11-15 14:59:53 +0800

    aa

--- importer.js
+++ importer.js
@@ -61,7 +61,7 @@ class Importer {
             var traceTranformer = getTraceTransform();
 
             console.log(currentFile, filePath, targetFilePath);
-            console.log(eadStream._readableState);
+            console.log(readStream._readableState);
 
             readStream.on('readable', () => {
                 console.log('readable', readStream._readableState)
